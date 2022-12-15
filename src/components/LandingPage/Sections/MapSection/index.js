b8643f24c132897edf2912f8bf817afc314b83d9
Gabriel-Giovani <gabriel.leite4@fatec.sp.gov.br>
2022-12-14 21:35:10 -0300

    Change addres in map section

--- src/components/LandingPage/Sections/MapSection/index.js
+++ src/components/LandingPage/Sections/MapSection/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 
 export default () => {
-    const iFrame =  '<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3658.7316480834224!2d-47.45074968451782!3d-23.506173365442752!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x94cf61946b7c4a3b%3A0x7c29ed6ccb499638!2sR.%20Santa%20Maria%2C%20231%20-%20Vila%20Hort%C3%AAncia%2C%20Sorocaba%20-%20SP%2C%2018020-216!5e0!3m2!1spt-BR!2sbr!4v1669938711615!5m2!1spt-BR!2sbr" width="100%" height="420px" style="border:0;" allowFullScreen="" loading="lazy" referrerPolicy="no-referrer-when-downgrade"></iframe>'
+    const iFrame =  '<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3657.884718685765!2d-47.44981128446641!3d-23.536648384695038!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x94cf61dfbc126251%3A0xd243212375206ada!2sAv.%20Ireno%20da%20Silva%20Ven%C3%A2ncio%2C%20199%20-%20Protestantes%2C%20Votorantim%20-%20SP%2C%2018111-100!5e0!3m2!1spt-BR!2sbr!4v1671064316615!5m2!1spt-BR!2sbr" width="100%" height="420px" style="border:0;" allowFullScreen="" loading="lazy" referrerPolicy="no-referrer-when-downgrade"></iframe>'
 
     return (
         <div dangerouslySetInnerHTML={ {__html:  iFrame}} />
